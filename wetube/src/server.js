e5ae08963732a38c411c83a99125acbeb3d42cd8
Eastar-DS <dororo0104@naver.com>
2022-01-17 00:36:18 +0900

    0117

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -1,9 +1,11 @@
 // const express = require("express")
 import express from "express"
+import morgan from "morgan"
 
 const PORT = 4002;
 
 const app = express()
+const logger = morgan("dev")
 
 // const handleHome = (req, res) => console.log("somebody is trying to go home")
 // app.get("/", () => console.log("somebody is trying to go home"))
@@ -12,6 +14,7 @@ const handleHome = (req, res) => {
     return res.send("Hello Newbie!")
 }
 
+app.use(logger)
 app.get("/", handleHome)
 
 
