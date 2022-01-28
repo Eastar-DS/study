62c8399abbac2177be4afa5f312d023e9044997d
Eastar-DS <dororo0104@naver.com>
2022-01-28 17:24:40 +0900

    wetube_3.2

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -1,24 +1,14 @@
-// const express = require("express")
 import express from "express"
-import morgan from "morgan"
+// const express = require("express")
 
-const PORT = 4002;
+const PORT = 4000;
 
 const app = express()
-const logger = morgan("dev")
 
-// const handleHome = (req, res) => console.log("somebody is trying to go home")
-// app.get("/", () => console.log("somebody is trying to go home"))
-const handleHome = (req, res) => {
-    // return res.end()
-    return res.send("Hello Newbie!")
-}
+const handlehome = () => console.log("Somebody try to get")
 
-app.use(logger)
-app.get("/", handleHome)
+app.get("/", handlehome)
 
-
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}❤`)
+const handleListening = () => console.log(`✅Server Listening on port http://localhost:${PORT}`)
 
 app.listen(PORT, handleListening)
