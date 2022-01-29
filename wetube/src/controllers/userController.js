2efadd90c62891092bb123019a079583550fe5ac
Eastar-DS <dororo0104@naver.com>
2022-01-29 14:05:37 +0900

    wetube_5.7Conditionals

--- wetube/src/controllers/userController.js
+++ wetube/src/controllers/userController.js
@@ -1,4 +1,7 @@
+
+
 export const join = (req,res) => res.send("Join")
+export const login = (req,res) => res.send("Login")
 
 export const edit = (req,res) => res.send("EditUser")
 export const remove = (req,res) => res.send("handleDelete")
