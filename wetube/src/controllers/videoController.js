49dd65d8044146e9a948178c4c39a726cbf34e9e
Eastar-DS <dororo0104@naver.com>
2022-01-29 22:05:15 +0900

    wetube_6.6_before_realDB

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,9 +1,47 @@
-const fakeUser = {
-    username:"Dubu",
-    loggedIn: false,
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2minutes ago",
+        views: 59,
+        id:1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2minutes ago",
+        views: 59,
+        id:2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2minutes ago",
+        views: 1,
+        id:3,
+    }
+]
+
+export const trending = (req,res) => {        
+    return res.render("home", {pageTitle : "Home", videos})
+}
+export const watch = (req,res) => {
+    // const id = req.params.id
+    const {id} = req.params
+    const video = videos[id-1]
+    return res.render("watch", {pageTitle : `Watching: ${video.title}`, video})
+}
+export const getEdit = (req,res) => {
+    const {id} = req.params
+    const video = videos[id-1]
+    return res.render("edit", {pageTitle : `Editing: ${video.title}`, video })
 }
 
-export const trending = (req,res) => res.render("home", {pageTitle : "Home", fakeUser : fakeUser})
-
-export const see = (req,res) => res.render("watch", {pageTitle : "Video"})
-export const edit = (req,res) => res.render("edit", {pageTitle : "Video"})
+export const postEdit = (req,res) => {
+    const { id } = req.params
+    const { title } = req.body
+    return res.redirect(`/videos/${id}`)
+}
